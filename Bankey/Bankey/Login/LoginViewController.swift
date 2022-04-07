b1a3d846606d67c847eb5c48ef317e07d7c13ca0
giovanebarreira <giovane_barreira@hotmail.com>
2022-04-07 08:25:42 -0300

    feat: Shake sign button if login fails

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -147,7 +147,12 @@ extension LoginViewController {
                   return
               }
         
-        if username == "" && password == "" {
+        if username.isEmpty || password.isEmpty {
+            configureView(withMessage: "Username / password cannot be blank")
+            return
+        }
+        
+        if username == "Giovane" && password == "Rosalia22" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         } else {
@@ -158,6 +163,18 @@ extension LoginViewController {
     private func configureView(withMessage message: String) {
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
+    }
+    
+    private func shakeButton() {
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0] // x positions
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1] // key frame timings
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
 }
 
